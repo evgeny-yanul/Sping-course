1b5c8cde2e699cd1bf04ae5677578876cd69bf26
golfstream83 <victul83@gmail.com>
2017-06-04 00:44:12 +0300

    lesson Spring Security 4, initial commit

---
+++ SpringSecurity_4/src/main/java/ru/javabegin/training/HomeController.java
@@ -0,0 +1,47 @@
+package ru.javabegin.training;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String mainPage() {
+
+		return "/content/user";
+
+	}
+
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage() {
+
+		return "/content/admin";
+
+	}
+
+	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username or password!");
+		}
+
+		model.setViewName("login");
+
+		return model;
+
+	}
+	
+}
