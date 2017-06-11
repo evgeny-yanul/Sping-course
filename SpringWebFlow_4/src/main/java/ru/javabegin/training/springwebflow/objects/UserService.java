ad1c39a813abebfcbcb494f5aa623e016fc0ac7b
golfstream83 <victul83@gmail.com>
2017-06-12 01:52:52 +0300

    Spring WebFlow, lesson 4 [data transfer from a form]

---
+++ SpringWebFlow_4/src/main/java/ru/javabegin/training/springwebflow/objects/UserService.java
@@ -0,0 +1,17 @@
+package ru.javabegin.training.springwebflow.objects;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+	public String checkUser(User user) {
+
+		if (user.getName() != null && user.getName().equals("user") && user.getPassword() != null && user.getPassword().equals("pass")) {
+			return "success";
+		} else {
+			return "failed";
+		}
+
+	}
+}
