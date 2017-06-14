4181a5f51411d457d29857bc815c995574c64d33
golfstream83 <victul83@gmail.com>
2017-06-14 22:34:48 +0300

    Spring WebFlow, lesson 6 [added scope]

---
+++ SpringWebFlow_6/src/main/java/ru/javabegin/training/springwebflow/objects/UserService.java
@@ -0,0 +1,37 @@
+package ru.javabegin.training.springwebflow.objects;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+	private ArrayList<User> userList = new ArrayList<User>();
+
+	public UserService() {
+		userList.add(new User("user", "pass"));
+	}
+
+	public boolean userExist(User user) {
+		if (userList.contains(user)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public  void createUser(User user){
+		userList.add(user);
+	}
+	
+	private boolean usernameExist(String username){
+		for (User user : userList) {
+			if (user.getName().equals(username)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
